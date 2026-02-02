21e9b1c29d3a8b8abfad1a6f3854b5f36c0ffc00
Naman Joshi <namanjoshi2403@gmail.com>
2026-02-02 23:03:47 +0530

    updated vocab

--- Code/config.py
+++ Code/config.py
@@ -1,3 +1,7 @@
 class config():
     INPUT_DATA = "Input\wiki-text-2.csv"
     CLEAN_TEXT_OUTPUT = r"Output\clean_text.txt"
+    CHUNK_SIZE = 500
+    MIN_FREQ = 5
+    vocab_path = "Tokenizer\Vocab"
+    
--- Code/tokanization.py
+++ Code/tokanization.py
@@ -12,12 +12,72 @@ config = config()
 clear_text = config.CLEAN_TEXT_OUTPUT
 
 ########### load small model 
-nlp = spacy.load("en_core_web_sm", disable=["ner", "parser", "tagger"])
+nlp = spacy.load("en_core_web_sm", disable=["ner", "parser", "tagger", "lemmatizer"])
 
 
 ########### read clear text
 with open(os.path.join(PROJECT_ROOT, clear_text), 'r', encoding="utf-8") as file:
     raw_text = file.read()
 
-doc= nlp(raw_text)
+
+
+########### Tokenization in chunks
 tokens = []
+chunk_size = config.CHUNK_SIZE
+
+for i in range(0, len(raw_text), chunk_size): 
+    chunk = raw_text[i : i + chunk_size]
+    doc= nlp(chunk)
+    
+
+    for token in doc:
+        if token.is_space:
+            continue
+
+        if token.is_alpha or token.is_digit or token.text in [".", ",", "!", "!", "?", "'", "-"]:
+            tokens.append(token.text)
+
+######## Vocab
+word_frequency = Counter(tokens)
+print("Vocabulary size (raw):", len(word_frequency))
+
+min_freq = config.MIN_FREQ
+filter_vocab = {word : freq for word, freq in word_frequency.items() if freq >= min_freq}
+    
+
+######## Create vocab mapping
+pad_token = "<PAD>"
+unk_token = "<UNK>"
+
+word_to_index = {
+    pad_token : 0,
+    unk_token : 1
+}
+
+index_to_word = {
+    0 : pad_token,
+    1 : unk_token
+} 
+
+for idx, value in enumerate(filter_vocab.keys(), start=2):
+    word_to_index[value] = idx
+    index_to_word[idx] = value
+
+vocab_size = len(word_to_index)
+print("Final vocabulary size (with special tokens):", vocab_size)
+
+encoded_raw_text = [word_to_index.get(word, word_to_index[unk_token]) for word in tokens]
+print("Encoded text length:", len(encoded_raw_text))
+print("UNK token count:", encoded_raw_text.count(word_to_index[unk_token]))
+
+######## Saving Vocab
+vocab_dir = os.path.join(PROJECT_ROOT, config.vocab_path)
+
+with open(os.path.join(vocab_dir, "word_to_index.pkl"), "wb") as file:
+    pickle.dump(word_to_index, file)
+
+with open(os.path.join(vocab_dir, "index_to_word.pkl"), "wb") as file:
+    pickle.dump(index_to_word, file)
+
+with open(os.path.join(encoded_raw_text, "encoded_text.pkl"), "wb") as file:
+    pickle.dump(encoded_raw_text, file)
